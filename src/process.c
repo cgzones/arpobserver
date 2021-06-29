7212c4da39298579684d788284765d356c1d9750
Christian Göttsche <cgzones@googlemail.com>
2021-06-29 12:16:11 +0200

    process: only set ip length if ip address is being set

--- src/process.c
+++ src/process.c
@@ -19,10 +19,9 @@ void process_arp(struct pkt *p)
 
 	arp = p->arp;
 
-	p->ip_len = IP4_LEN;
-
 	if (be32toh(*(const uint32_t *)arp->arp_spa) == INADDR_ANY) {
 		p->l2_addr = arp->arp_sha;
+		p->ip_len = IP4_LEN;
 		p->ip_addr = arp->arp_tpa;
 		p->origin = ARP_ACD;
 		save_pairing(p);
@@ -33,6 +32,7 @@ void process_arp(struct pkt *p)
 
 	if (arp_opcode == ARPOP_REQUEST) {
 		p->l2_addr = arp->arp_sha;
+		p->ip_len = IP4_LEN;
 		p->ip_addr = arp->arp_spa;
 		p->origin = ARP_REQ;
 		save_pairing(p);
@@ -41,6 +41,7 @@ void process_arp(struct pkt *p)
 
 	if (arp_opcode == ARPOP_REPLY) {
 		p->l2_addr = arp->arp_sha;
+		p->ip_len = IP4_LEN;
 		p->ip_addr = arp->arp_spa;
 		p->origin = ARP_REP;
 		save_pairing(p);
@@ -54,10 +55,9 @@ void process_ns(struct pkt *p)
 {
 	assert(p);
 
-	p->ip_len = IP6_LEN;
-
 	if (IN6_IS_ADDR_UNSPECIFIED(&p->ip6->ip6_src)) {
 		p->l2_addr = p->ether->ether_shost;
+		p->ip_len = IP6_LEN;
 		p->ip_addr = (const uint8_t *)&p->ns->nd_ns_target;
 		p->origin = ND_DAD;
 		save_pairing(p);
@@ -66,6 +66,7 @@ void process_ns(struct pkt *p)
 
 	if (p->opt_slla) {
 		p->l2_addr = (const uint8_t *)(p->opt_slla + 1);
+		p->ip_len = IP6_LEN;
 		p->ip_addr = (const uint8_t *)&p->ip6->ip6_src;
 		p->origin = ND_NS;
 		save_pairing(p);
@@ -79,13 +80,12 @@ void process_na(struct pkt *p)
 {
 	assert(p);
 
-	p->ip_len = IP6_LEN;
-
 	/* RFC4861 allows the 'Source link-layer address' to be omitted. */
 	if (!p->opt_tlla)
 		return;
 
 	p->l2_addr = (const uint8_t *)(p->opt_tlla + 1);
+	p->ip_len = IP6_LEN;
 	p->ip_addr = (const uint8_t *)&p->na->nd_na_target;
 	p->origin = ND_NA;
 	save_pairing(p);
@@ -95,13 +95,12 @@ void process_ra(struct pkt *p)
 {
 	assert(p);
 
-	p->ip_len = IP6_LEN;
-
 	/* RFC4861 allows the 'Source link-layer address' to be omitted. */
 	if (!p->opt_slla)
 		return;
 
 	p->l2_addr = (const uint8_t *)(p->opt_slla + 1);
+	p->ip_len = IP6_LEN;
 	p->ip_addr = (const uint8_t *)&p->ip6->ip6_src;
 	p->origin = ND_RA;
 	save_pairing(p);
@@ -111,14 +110,13 @@ void process_rs(struct pkt *p)
 {
 	assert(p);
 
-	p->ip_len = IP6_LEN;
-
 	/* RFC4861 specifies the 'Source link-layer address' SHOULD be included.
 	 * Ignore otherwise. */
 	if (!p->opt_slla)
 		return;
 
 	p->l2_addr = (const uint8_t *)(p->opt_slla + 1);
+	p->ip_len = IP6_LEN;
 	p->ip_addr = (const uint8_t *)&p->ip6->ip6_src;
 	p->origin = ND_RS;
 	save_pairing(p);
